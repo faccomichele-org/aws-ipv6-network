2edfa53e00181e7c153f7ed2bd2016128ad6975a
Michele Facco <faccomichele@gmail.com>
2026-08-15 13:59:39 +0700

    fix

--- definitions.tf
+++ definitions.tf
@@ -26,7 +26,7 @@ locals {
     Project = local.project_name
   })
 
-  logging_bucket_name = var.logging_bucket_name != null ? var.logging_bucket_name : "flow-logs-${data.aws_caller_identity.current.account_id}-${local.aws_region}-${local.environment}"
+  logging_bucket_name = "${local.project_name}-flow-logs-${local.environment}-${local.aws_region}-${data.aws_caller_identity.current.account_id}"
 
   flow_log_format = "$${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${vpc-id} $${subnet-id} $${instance-id} $${tcp-flags} $${type} $${pkt-srcaddr} $${pkt-dstaddr} $${region} $${az-id} $${sublocation-type} $${sublocation-id} $${pkt-src-aws-service} $${pkt-dst-aws-service} $${flow-direction} $${traffic-path} $${ecs-cluster-arn} $${ecs-cluster-name} $${ecs-service-name} $${ecs-task-arn} $${ecs-task-id} $${ecs-task-definition-arn} $${ecs-container-instance-arn} $${ecs-container-instance-id} $${ecs-container-id} $${ecs-second-container-id} $${reject-reason} $${resource-id} $${encryption-status} $${instance-tag} $${instance-tag-2} $${interface-tag} $${interface-tag-2} $${asg-tag} $${asg-tag-2} $${interface-type} $${next-hop-interface-id} $${next-hop-subnet-id} $${next-hop-az-id} $${next-hop-vpc-id} $${next-hop-interface-type}"
 }
