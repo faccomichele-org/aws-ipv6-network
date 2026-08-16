7f5d5f1c0dd789ea1ea560b8b83db52541e9fb70
Michele Facco <faccomichele@gmail.com>
2026-08-16 15:38:39 +0700

    fix outputs to ssm (#13)

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,19 @@
 output "vpc_id" {
   description = "ID of the IPv6-enabled VPC."
-  value       = aws_vpc.this.id
+  value       = aws_ssm_parameter.vpc_id.name
 }
 
 output "vpc_ipv6_cidr_block" {
   description = "AWS-assigned IPv6 CIDR block for the VPC."
-  value       = aws_vpc.this.ipv6_cidr_block
+  value       = aws_ssm_parameter.vpc_ipv6_cidr_block.name
 }
 
 output "public_subnet_ids" {
   description = "IDs of the three public IPv6-native subnets."
-  value       = [for subnet in aws_subnet.public : subnet.id]
+  value       = aws_ssm_parameter.public_subnet_ids.name
 }
 
 output "private_subnet_ids" {
   description = "IDs of the three private IPv6-native subnets."
-  value       = [for subnet in aws_subnet.private : subnet.id]
-}
-
-output "flow_log_id" {
-  description = "ID of the VPC flow log, if enabled."
-  value       = try(aws_flow_log.vpc[0].id, null)
-}
-
-output "logging_bucket_name" {
-  description = "Name of the S3 bucket used for flow logs and DNS query logs."
-  value       = aws_s3_bucket.logging.id
-}
-
-output "logging_bucket_arn" {
-  description = "ARN of the S3 bucket used for flow logs and DNS query logs."
-  value       = aws_s3_bucket.logging.arn
-}
-
-output "resolver_query_log_config_id" {
-  description = "ID of the Route 53 Resolver query log configuration, if enabled."
-  value       = try(aws_route53_resolver_query_log_config.vpc[0].id, null)
-}
-
-output "guardduty_detector_id" {
-  description = "ID of the GuardDuty detector, if enabled."
-  value       = try(aws_guardduty_detector.this[0].id, null)
-}
-
-output "athena_database_name" {
-  description = "Name of the Glue database containing the flow log and resolver log tables, if enabled."
-  value       = try(aws_glue_catalog_database.logging[0].name, null)
+  value       = aws_ssm_parameter.private_subnet_ids.name
 }
